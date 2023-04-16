f421f134209c1aa72150793e4a43584ce61196c7
NigthBus <sngiagom@gmail.com>
2023-04-15 21:21:06 -0500

    Se crea view profile

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -4,66 +4,87 @@ import { useState, useEffect } from "react";
 import { Toaster, toast } from "react-hot-toast";
 
 export default function Home() {
-  return (
-    <div className="min-h-screen flex items-center justify-center bg-gray-100">
-      <div className="bg-white shadow-md rounded-md p-8 max-w-md w-full">
-        <h2 className="text-2xl font-semibold mb-6 text-center">Registrarse</h2>
-        <form>
-          <div className="grid gap-4 grid-cols-2">
-            <div className="mb-4">
-              <label htmlFor="firstName" className="block mb-1 font-medium">Nombre</label>
-              <input
-                type="text"
-                id="firstName"
-                className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-blue-500"
-                placeholder="Tu nombre"
-              />
-            </div>
-            <div className="mb-4">
-              <label htmlFor="lastName" className="block mb-1 font-medium">Apellido</label>
-              <input
-                type="text"
-                id="lastName"
-                className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-blue-500"
-                placeholder="Tu apellido"
-              />
-            </div>
-          </div> 
-          <div className="mb-4">
-            <label htmlFor="tel" className="block mb-1 font-medium">Celular</label>
-            <input
-              type="text" inputmode="numeric" pattern="[0-9]*"
-              id="phone"
-              className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-blue-500"
-              placeholder="Celular" 
-            /> 
-          </div> 
-          <div className="mb-4">
-            <label htmlFor="email" className="block mb-1 font-medium">Correo electrónico</label>
-            <input
-              type="email"
-              id="email"
-              className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-blue-500"
-              placeholder="Tu correo electrónico"
-            />
+  return ( 
+    <div className="min-h-screen  justify-center bg-gray-100">
+      <div class="max-w-2xl mx-auto px-4 py-8">
+      <div class="bg-white rounded-lg shadow-lg">
+          <div class="flex items-center justify-center bg-[#1F6768] rounded-t-lg px-4 py-8">
+              <img class="w-32 h-32 rounded-full border-4 border-white"
+                  src="https://via.placeholder.com/150" alt="Foto de Perfil"/>
           </div>
-          <div className="mb-4">
-            <label htmlFor="password" className="block mb-1 font-medium">Contraseña</label>
-            <input
-              type="password"
-              id="password"
-              className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-blue-500"
-              placeholder="Tu contraseña"
-            />
+          <div class="px-6 py-4">
+              <h2 class="text-2xl font-bold text-gray-800">Perfil del docente</h2>
+              <p class="text-gray-600"></p>
           </div>
-          <button
-            type="submit"
-            className="w-full py-2 px-4 bg-blue-500 hover:bg-blue-600 text-white rounded-md font-semibold focus:outline-none"
-          >
-            Registrarse
-          </button>
-        </form>
-      </div>
-    </div>
+          <div class="px-6 py-4">
+              <form class="space-y-4">
+                  <div>
+                    <label for="nombre" class="block text-gray-800 font-bold mb-2">Nombre</label>
+                    <input
+                      type="text"
+                      id="firstName"
+                      className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none hover:border-[#1F6768] text-[#000000]"
+                      value="Nombre del docente"
+                    />
+                  </div> 
+                  <div>
+                    <label for="nombre" class="block text-gray-800 font-bold mb-2">Apellidos</label>
+                    <input
+                      type="text"
+                      id="firstName"
+                      className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none hover:border-[#1F6768] text-[#000000]"
+                      value="Apellidos del docente"
+                    />
+                  </div> 
+                  <div>
+                    <label for="nombre" class="block text-gray-800 font-bold mb-2">Cédula</label>
+                    <input
+                      type="text"
+                      id="firstName"
+                      className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none hover:border-[#1F6768] text-[#000000]"
+                      value="Cédula del docente"
+                    />
+                  </div> 
+                  <div>
+                    <label for="nombre" class="block text-gray-800 font-bold mb-2">Celular</label>
+                    <input
+                      type="text"
+                      id="firstName"
+                      className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none hover:border-[#1F6768] text-[#000000]"
+                      value="Celular del docente"
+                    />
+                  </div> 
+                  <div>
+                    <label for="nombre" class="block text-gray-800 font-bold mb-2">Correo electrónico</label>
+                    <input
+                      type="text"
+                      id="firstName"
+                      className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none hover:border-[#1F6768] text-[#000000]"
+                      value="Correo del docente"
+                    />
+                  </div> 
+                  <div class="relative"> 
+                    <label for="nombre" class="block text-gray-800 font-bold mb-2">Valor de la hora</label>   
+                    <span class="absolute left-3 bottom-2.5 text-[#000000]">$
+                    </span>
+                    <input
+                      type="text"    
+                      inputmode="numeric" 
+                      pattern="[0-9]*"  
+                      id="phone"
+                      class="w-full px-3 py-2 pl-10 pr-4 border border-gray-300 rounded-md focus:outline-none focus:border-[#1F6768] text-[#000000]" 
+                      placeholder="Ingrese su valor"
+                    />  
+                  </div>  
+                  <button 
+                    type="submit"
+                    className="w-full py-2 px-4 bg-[#1F6768] hover:bg-[#EE2737] text-white rounded-md font-semibold focus:outline-none">
+                    Actualizar
+                  </button>   
+              </form>  
+            </div> 
+      </div>     
+      </div>    
+    </div>     
   );
 }
--- client/src/pages/signup.js
+++ client/src/pages/signup.js
@@ -4,104 +4,80 @@ import { useState, useEffect } from "react";
 import { Toaster, toast } from "react-hot-toast";
 
 export default function Home() {
-  const [teachers, setTeachers] = useState([]);
-  const url = "http://localhost:4000/teachers";
-  useEffect(() => {
-    const fetchData = async () => {
-      const result = await axios.get(url);
-      setTeachers(result.data);
-      console.log(teachers);
-    };
-
-    fetchData();
-  }, []);
-  const handleDelete = (id) => {
-    console.log(id)
-    toast((t) => (
-      <div>
-        <p>
-          ¿Quieres eliminar a este profesor?
-          {id}
-        </p>
-        <div clasName="flex">
-          <button className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded mx-2">
-            Borrar
-          </button>
-          <button
-            className="bg-blue-300 hover:bg-blue-400 text-white font-bold py-2 px-4 rounded"
-            onClick={() => toast.dismiss(t.id)}
-          >
-            Cancelar
-          </button>
-        </div>
-      </div>
-    ));
-  }; 
   return (
-    <div>
-      <table className="table w-full border-collapse">
-        <thead>
-          <tr>
-            <th className="p-7 font-bold uppercase bg-gray-200 text-gray-600 border border-gray-300 hidden lg:table-cell">
-              Documento
-            </th>
-            <th className="p-7 font-bold uppercase bg-gray-200 text-gray-600 border border-gray-300 hidden lg:table-cell">
-              Nombre
-            </th>
-            <th className="p-7 font-bold uppercase bg-gray-200 text-gray-600 border border-gray-300 hidden lg:table-cell">
-              Apellido
-            </th>
-            <th className="p-7 font-bold uppercase bg-gray-200 text-gray-600 border border-gray-300 hidden lg:table-cell">
-              Materia
-            </th>
-            <th className="p-7 font-bold uppercase bg-gray-200 text-gray-600 border border-gray-300 hidden lg:table-cell">
-              Email
-            </th>
-            <th className="p-7 font-bold uppercase bg-gray-200 text-gray-600 border border-gray-300 hidden lg:table-cell">
-              Celular
-            </th>
-            <th className="p-7 font-bold uppercase bg-gray-200 text-gray-600 border border-gray-300 hidden lg:table-cell w-1/5">
-              Acciones
-            </th>
-          </tr>
-        </thead>
-        <tbody>
-          {teachers.map((teacher) => (
-            <tr className="bg-white lg:hover:bg-gray-100" key={teacher.id}>
-              <td class="p-7 border border-gray-300 hidden lg:table-cell">
-                {teacher.ced}
-              </td>
-              <td className="p-7 border border-gray-300 hidden lg:table-cell">
-                {teacher.name}
-              </td>
-              <td className="p-7 border border-gray-300 hidden lg:table-cell">
-                {teacher.lastname}
-              </td>
-              <td className="p-7 border border-gray-300 hidden lg:table-cell">
-                {teacher.subject}
-              </td>
-              <td className="p-7 border border-gray-300 hidden lg:table-cell">
-                {teacher.email}
-              </td>
-              <td className="p-7 border border-gray-300 hidden lg:table-cell">
-                {teacher.cell}
-              </td>
-              <td className="p-7 border border-gray-300 hidden lg:table-cell flex space-x-2">
-                <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-                  Editar
-                </button>
-                <button
-                  class="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded"
-                  onClick={() => handleDelete(teacher._id)}
-                >
-                  Borrar
-                </button>
-              </td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-      <Toaster />
+    <div className="min-h-screen flex items-center justify-center bg-gray-100 text-[#000000]">
+      <div className="bg-white shadow-md rounded-md p-8 max-w-md w-full">
+        <h2 className="text-2xl font-semibold mb-6 text-center ">Registrarse</h2>
+        <form>
+          <div className="grid gap-4 grid-cols-2">
+            <div className="mb-4">
+              <label htmlFor="firstName" className="block mb-1 font-medium">Nombre</label>
+              <input
+                type="text"
+                id="firstName"
+                className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-[#EE2737]"
+                placeholder="Tu nombre"
+              />
+            </div>
+            <div className="mb-4">
+              <label htmlFor="lastName" className="block mb-1 font-medium">Apellidos</label>
+              <input
+                type="text"
+                id="lastName"
+                className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-[#EE2737]"
+                placeholder="Tus apellidos"
+              />
+            </div>
+          </div>  
+          <div className="mb-4">
+            <label htmlFor="phone" className="block mb-1 font-medium">Cédula</label>
+            <input
+              type="text"    
+              inputmode="numeric" 
+              pattern="[0-9]*" 
+              id="phone"
+              className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-[#EE2737]"
+              placeholder="Tu cédula" 
+            /> 
+          </div> 
+          <div className="mb-4">
+            <label htmlFor="phone" className="block mb-1 font-medium">Celular</label>
+            <input
+              type="text"    
+              inputmode="numeric" 
+              pattern="[0-9]*" 
+              id="phone"
+              className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-[#EE2737]"
+              placeholder="Tu celular" 
+            /> 
+          </div>  
+          <div className="mb-4">
+            <label htmlFor="email" className="block mb-1 font-medium">Correo electrónico</label>
+            <input
+              type="email"
+              id="email"
+              className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-[#EE2737]"
+              placeholder="Tu correo electrónico"
+            />
+          </div>
+          <div className="mb-4">
+            <label htmlFor="password" className="block mb-1 font-medium">Contraseña</label>
+            <input
+              type="password"
+              id="password"
+              className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-[#EE2737]"
+              placeholder="Tu contraseña"
+            />
+          </div>
+          <button
+            type="submit"
+            className="w-full py-2 px-4 bg-[#EE2737] hover:bg-[#1F6768] text-white rounded-md font-semibold focus:outline-none"
+          >
+            Registrarse
+          </button>   
+          <p class="text-gray-600 text-center">Inicia sesión <a href="#" class="text-[#EE2737] hover:text-[#1F6768]">aquí.</a></p>
+        </form>
+      </div>
     </div>
   );
 }
