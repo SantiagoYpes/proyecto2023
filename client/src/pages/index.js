8cb634e7b306b598e4dc15ae3992dd9c9ddc1870
Santiago Franco <ktns0930@gmail.com>
2023-04-16 19:04:57 -0500

    Home page structure created

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -1,5 +1,16 @@
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
-  return <div className={styles.container}></div>;
+  return (
+    <div className={styles.body}>
+      <div className={styles.bgImage}></div>
+      <div className={styles.mainContainer}>
+        <h1>ADMIN-360</h1>
+        <h3>Espacio unico para profes</h3>
+        <button class="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded">
+          Button
+        </button>
+      </div>
+    </div>
+  );
 }
