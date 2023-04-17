49a24b249db7e4ba0c856d5210cb2ee4d87d9239
Santiago Franco <ktns0930@gmail.com>
2023-04-16 20:03:45 -0500

    Full schema for homepage is ready

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -12,6 +12,14 @@ export default function Home() {
         </button>
       </div>
       <footer className={styles.footer}>
+        <div className={styles.footerContainer}>
+          <h2>¡Sitio único y exclusivo para docentes!</h2>
+          <p>
+            Desde aqui podras observar información acerca de los cursos para los
+            cuales has sido contratado. Además tendrás la posibilidad de
+            gestionar tu contrato y demas informacion laboral y profesional
+          </p>
+        </div>
       </footer>
     </div>
   );
