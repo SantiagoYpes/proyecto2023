039a8a3ce06060001f8399f9cf208a4f607cfe96
Santiago Franco <ktns0930@gmail.com>
2023-04-18 01:40:49 -0500

    Contact form structure created

--- client/src/pages/contact.js
+++ client/src/pages/contact.js
@@ -48,15 +48,17 @@ export default function Contact() {
       <footer className={styles.footer}>
         <div className={styles.footerContainer}>
           <h2 className="">Contáctantos</h2>
-          <form className="flex flex-row gap-4">
-            <input
-              className="mb-1 bg-gray-100 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 cursor-not-allowed dark:bg-white-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-gray-400 dark:focus:ring-blue-500 dark:focus:border-blue-500"
-              placeholder="NOMBRE"
-            ></input>
-            <input
-              placeholder="E-MAIL"
-              className="mb-1 bg-gray-100 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 cursor-not-allowed dark:bg-white-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-gray-400 dark:focus:ring-blue-500 dark:focus:border-blue-500"
-            ></input>
+          <form className="flex flex-col gap-4">
+            <div className="flex flex-row gap-4">
+              <input
+                className="mb-1 bg-gray-100 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 cursor-not-allowed dark:bg-white-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-gray-400 dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                placeholder="NOMBRE"
+              ></input>
+              <input
+                placeholder="E-MAIL"
+                className="mb-1 bg-gray-100 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 cursor-not-allowed dark:bg-white-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-gray-400 dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              ></input>
+            </div>
             <input
               placeholder="MENSAJE"
               className="mb-1 bg-gray-100 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 cursor-not-allowed dark:bg-white-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-gray-400 dark:focus:ring-blue-500 dark:focus:border-blue-500"
