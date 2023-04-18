0aff000e56e788fc4998913aeb71b2aedd70ae04
Santiago Franco <ktns0930@gmail.com>
2023-04-17 23:19:54 -0500

    Main page is partial finished, rest minimun additions.

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -5,16 +5,16 @@ export default function Home() {
     <div className={styles.body}>
       <div className={styles.bgImage}></div>
       <div className={styles.mainContainer}>
-        <h1>ADMIN-360</h1>
-        <h3>Espacio unico para profes</h3>
+        <h1 className={styles.title}>ADMIN-360</h1>
+        <h3 className={styles.subtitle}>Espacio unico para profes</h3>
         <button class="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded">
-          Button
+          Acerca de la U
         </button>
       </div>
       <footer className={styles.footer}>
         <div className={styles.footerContainer}>
-          <h2>¡Sitio único y exclusivo para docentes!</h2>
-          <p>
+          <h2 className={styles.subtitle}>¡Sitio único y exclusivo para docentes!</h2>
+          <p className={styles.text}>
             Desde aqui podras observar información acerca de los cursos para los
             cuales has sido contratado. Además tendrás la posibilidad de
             gestionar tu contrato y demas informacion laboral y profesional
