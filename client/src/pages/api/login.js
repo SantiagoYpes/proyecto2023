a2b9149149b04e25782bf638e7092422ef4546eb
Santiago Franco <ktns0930@gmail.com>
2023-04-25 16:40:28 -0500

    Now our token is setted into cookies

--- client/src/pages/api/login.js
+++ client/src/pages/api/login.js
@@ -9,8 +9,24 @@ export default function LoginService(req, res) {
 
   dataFromDB = req.body.data;
   if (dataFromDB["type"] !== "") {
-    console.log(dataFromDB);
+    const token = jwt.sign(
+      {
+        exp: Math.floor(Date.now() / 1000) + 60 * 60 * 24 * 30,
+        type: dataFromDB.type,
+      },
+      "secret"
+    );
 
+    const serialized = serialize("userToken", token, {
+        httOnly: true, //Evita que la coockie se muestre en el front.
+        secure: process.env.NODE_ENV === "production", //Valida en que ambiente esta.
+        sameSite: "strict", // para servicios externos como el backen separado usar el valor none.
+        maxAge: 1000 + 60 * 60 * 24 * 30,
+        path: "/",
+      });
+
+    console.log(serialized);
+    res.setHeader("Set-Cookie", serialized);
     return res.json("Creado correctamente");
   }
   return res.status(401).json({ error: "No se pudo autenticar con JWT" });
