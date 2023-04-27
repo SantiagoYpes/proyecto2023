0dde30abda6ba329628d2b4e9ef3bce997ee4927
NigthBus <sngiagom@gmail.com>
2023-04-26 22:18:21 -0500

    Se ajustan arreglos de la foto de perfil

--- client/src/components/PhotoUpload.js
+++ client/src/components/PhotoUpload.js
@@ -20,14 +20,13 @@ function PhotoUpload() {
 
   return (
     <div className="flex flex-col items-center">
-      <label htmlFor="photo-upload" className="relative cursor-pointer bg-gray-300 rounded-md font-medium text-white hover:bg-gray-400 focus-within:outline-none focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-indigo-500 px-4 py-2">
+      {photoPreview && (
+        <img src={photoPreview} alt="Vista previa de la foto de perfil" className="object-contain w-52 h-52 object-cover rounded-full border-4 border-white mt-4" />
+      )} 
+      <label htmlFor="photo-upload" className="relative cursor-pointer mt-3 bg-[#EE2737] hover:bg-red-700 rounded-md font-medium text-white focus-within:outline-none focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-indigo-500 px-4 py-2">
         Adjuntar foto
         <input id="photo-upload" name="photo-upload" type="file" className="sr-only" onChange={handlePhotoUpload} />
       </label>
-
-      {photoPreview && (
-        <img src={photoPreview} alt="Vista previa de la foto de perfil" className="object-contain w-52 h-52 object-cover rounded-full border-4 border-white mt-4" />
-      )}
     </div>
   );
 }
