cae3c99e27a7728781c493ddff988c2af6f2eef9
NigthBus <sngiagom@gmail.com>
2023-05-23 00:47:05 -0500

    btns funcionando

--- client/src/pages/contact.js
+++ client/src/pages/contact.js
@@ -32,24 +32,35 @@ export default function TableContractUser() {
         <div className="min-h-screen items-center justify-center bg-gray-100 text-[#000000]">
             <ComplexNavbar />
             <br></br>
-            <div class="flex space-x-4">
-                <div class="bg-white rounded-lg text-center shadow-lg p-6">
+            <center>
+                <Typography
+                    variant="h2"
+                    className="text-black-500 p-7 text-4xl  "
+                >
+                    ¡TENEMOS VARIAS MANERAS DE DARTE SOPORTE!
+                </Typography>
+            </center>
+            <div class="flex space-x-1 m-6">
+                <div class="bg-white rounded-lg text-center shadow-lg p-6 border hover:scale-105 hover:border-[#1F6768]">
                     <h2 class="text-xl font-semibold">WhatsApp</h2>
                     <p class="mb-5 mt-3">¡Hablanos a nuestra línea única de atención de WhatsApp para brindarte el mejor apoyo!</p>
                     <a href="https://api.whatsapp.com/send/?phone=3245301146" class="bg-[#EE2737] hover:bg-[#1F6768] text-white font-bold py-2 px-4 rounded">
                         Ir al chat
                     </a>
                 </div>
-                <div class="bg-white rounded-lg text-center shadow-lg p-6">
+                <div class="bg-white rounded-lg text-center shadow-lg p-6 border hover:scale-105 hover:border-[#1F6768]">
                     <h2 class="text-xl font-semibold">Correo electrónico</h2>
                     <p class="mb-5 mt-3">¡Hablanos a nuestra línea única de atención de correo electrónico para brindarte el mejor apoyo!</p>
                     <a href="mailto:smontoya660@soyudemedellin.edu.co" class="bg-[#EE2737] hover:bg-[#1F6768] text-white font-bold py-2 px-4 rounded">
                         Ir al correo
                     </a>
                 </div>
-                <div class="bg-white rounded-lg text-center shadow-lg p-6">
-                    <h2 class="text-xl font-semibold">Tarjeta 3</h2>
-                    <p class="mb-5 mt-3">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla sagittis turpis at elementum ultrices.</p>
+                <div class="mr-9 bg-white rounded-lg text-center shadow-lg p-6 border hover:scale-105 hover:border-[#1F6768]">
+                    <h2 class="text-xl font-semibold">Teléfono</h2>
+                    <p class="mb-5 mt-3">¡Hablanos a nuestra línea única de atención telefónica para brindarte el mejor apoyo!</p>
+                    <a href="tel:+573245301146" class="bg-[#EE2737] hover:bg-[#1F6768] text-white font-bold py-2 px-4 rounded">
+                        Ir al correo
+                    </a>
                 </div>
             </div>
             <Footer />
