e80eb0f1bcb618c4ef6513176869776dff20acf8
SantiagoYpes <santiyezu8@hotmail.com>
2023-05-25 00:24:30 -0500

    Update Cors

--- server/app.js
+++ server/app.js
@@ -5,20 +5,30 @@ import { dirname, join } from "path";
 import { fileURLToPath } from "url";
 import fileUpload from "express-fileupload";
 import cors from "cors";
-const app = express()
+const app = express();
 
 const corsOptions = {
-    origin: 'http://localhost:3000',
-    methods: 'GET,POST,DELETE',
-    allowedHeaders: 'Content-Type',
-  };
-app.use(express.json())
-app.use(fileUpload({
-  useTempFiles: true,
-  tempFileDir: './upload'
-}))
-app.use(cors(corsOptions));
-app.use(adminRoutes)
-app.use(userRoutes)
+  origin: "http://localhost:3000",
+  methods: "GET,POST,DELETE",
+  allowedHeaders: "Content-Type",
+};
 
-export default app
+const corsOptions1 = {
+  origin: "http://34.238.85.78:3000",
+  methods: "GET,POST,DELETE",
+  allowedHeaders: "Content-Type",
+};
+
+app.use(express.json());
+app.use(
+  fileUpload({
+    useTempFiles: true,
+    tempFileDir: "./upload",
+  })
+);
+app.use(cors(corsOptions));
+app.use(cors(corsOptions1));
+app.use(adminRoutes);
+app.use(userRoutes);
+
+export default app;
