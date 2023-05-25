4c432aa2c58996cd42f09ea74f15e59753ef9422
SantiagoYpes <santiyezu8@hotmail.com>
2023-05-25 00:39:41 -0500

    Me devolví

--- server/app.js
+++ server/app.js
@@ -13,12 +13,6 @@ const corsOptions = {
   allowedHeaders: "Content-Type",
 };
 
-const corsOptions1 = {
-  origin: "http://34.238.85.78:3000",
-  methods: "GET,POST,DELETE",
-  allowedHeaders: "Content-Type",
-};
-
 app.use(express.json());
 app.use(
   fileUpload({
@@ -27,7 +21,6 @@ app.use(
   })
 );
 app.use(cors(corsOptions));
-app.use(cors(corsOptions1));
 app.use(adminRoutes);
 app.use(userRoutes);
 
